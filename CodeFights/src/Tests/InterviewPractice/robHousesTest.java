bbecf123f1d06b1f8b7a99771aad5d05d5b221da
Trung Dang <tdang229@uw.edu>
2017-05-19 10:12:40 -0700

    added arcade first level and test

---
+++ CodeFights/src/Tests/InterviewPractice/robHousesTest.java
@@ -0,0 +1,17 @@
+package InterviewPractice;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Trung on 5/19/2017.
+ */
+public class robHousesTest {
+    @Test
+    public void houseRobber() throws Exception {
+
+
+    }
+
+}
